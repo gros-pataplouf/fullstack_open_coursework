99c349b29ba89a07164134e32908b575be02275e
gros-pataplouf <sceres@pm.me>
2023-11-03 22:50:51 +0100

    finish 1.3 to 1.5

---
+++ part_1/1.1-1.2_courseinfo/src/App.jsx
@@ -0,0 +1,62 @@
+const Header = (props) => {
+// eslint-disable-next-line react/prop-types
+  const {course} = props;
+  return <h1>{course}</h1>
+}
+
+const Part = (props) => {
+  // eslint-disable-next-line react/prop-types
+  // eslint-disable-next-line react/prop-types
+  const {name, exercises} = props.parts;
+  return <p>{name}&nbsp;{exercises}</p>
+}
+
+const Content = (props) => {
+  // eslint-disable-next-line react/prop-types
+    const courseArray = props.parts;
+        return courseArray.map(part => {
+      return <Part key={courseArray.indexOf(part)} parts={part}/>
+      }
+    )
+  }
+
+  const Total = (props) => {
+      // eslint-disable-next-line react/prop-types
+    const courseArray = props.parts;
+    console.log(courseArray)
+          // eslint-disable-next-line react/prop-types
+    const total = courseArray.reduce((acc, current) => {
+      return acc + current.exercises
+    }, 0)
+    return <p>Number of exercises {total}</p>
+
+  }
+
+
+
+const App = () => {
+  const course = 'Half Stack application development';
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+  return (
+    <div>
+      <Header course={course}/>
+      <Content parts={parts}/>
+      <Total parts={parts}/>
+    </div>
+  )
+}
+
+export default App
