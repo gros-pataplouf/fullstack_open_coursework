b9b6085a68676217b5ee140e2e4c8b9d4208ee3e
gros-pataplouf <sceres@pm.me>
2023-11-26 18:59:59 +0100

    unicafé step 4

--- part_1/1.6-1.11_unicafé/src/App.jsx
+++ part_1/1.6-1.11_unicafé/src/App.jsx
@@ -5,6 +5,26 @@ const Button = ({onClickHandler, text}) => {
   return <button onClick={onClickHandler}>{text}</button>
 }
 
+const Statistics = ({feedback}) => {
+  return (
+    <div>
+
+    <h1>statistics</h1>
+    {feedback.bad.count + feedback.good.count + feedback.neutral.count === 0?
+      <p>No feedback given</p> :
+      <>
+      <p>GOOD {feedback.good.count}</p>
+    <p>NEUTRAL {feedback.neutral.count}</p>
+    <p>BAD {feedback.bad.count}</p>
+    <p>ALL {feedback.good.count + feedback.neutral.count + feedback.bad.count}</p>
+    <p>AVERAGE {((feedback.good.value*feedback.good.count + feedback.bad.value*feedback.bad.count)/(feedback.good.count + feedback.neutral.count + feedback.bad.count)).toFixed(2)}</p>
+    <p>POSITIVE {((feedback.good.value*feedback.good.count)/(feedback.good.count + feedback.neutral.count + feedback.bad.count)*100).toFixed(2)}%</p>
+    </>
+    }
+    </div>
+  )
+}
+
 
 const App = () => {
   // save clicks of each button to its own state
@@ -39,14 +59,7 @@ const App = () => {
       <Button onClickHandler={handleFeedback("good")} text="good"/>
       <Button onClickHandler={handleFeedback("neutral")} text="neutral"/>
       <Button onClickHandler={handleFeedback("bad")} text="bad"/>
-      <h1>statistics</h1>
-    <p>GOOD {feedback.good.count}</p>
-    <p>NEUTRAL {feedback.neutral.count}</p>
-    <p>BAD {feedback.bad.count}</p>
-    <p>ALL {feedback.good.count + feedback.neutral.count + feedback.bad.count}</p>
-    <p>AVERAGE {((feedback.good.value*feedback.good.count + feedback.bad.value*feedback.bad.count)/(feedback.good.count + feedback.neutral.count + feedback.bad.count)).toFixed(2)}</p>
-    <p>POSITIVE {((feedback.good.value*feedback.good.count)/(feedback.good.count + feedback.neutral.count + feedback.bad.count)*100).toFixed(2)}%</p>
-
+      <Statistics feedback={feedback}/>
     </div>
 
   )
