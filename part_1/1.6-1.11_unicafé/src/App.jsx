fc2f57ec6db50d82233ecc072f24983e8150489a
gros-pataplouf <sceres@pm.me>
2023-11-26 18:23:45 +0100

    unicafé step 1

--- part_1/1.6-1.11_unicafé/src/App.jsx
+++ part_1/1.6-1.11_unicafé/src/App.jsx
@@ -1,34 +1,50 @@
+/* eslint-disable react/prop-types */
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Button = ({onClickHandler, text}) => {
+  return <button onClick={onClickHandler}>{text}</button>
+}
+
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [feedback, setFeedback ] = useState({
+    good : 0, 
+    neutral: 0, 
+    bad: 0
+  })
+
+  const handleFeedback = (how) => {
+      const innerFunction = () => {
+        switch(how) {
+          case "good":
+            setFeedback({...feedback, good: feedback.good + 1});
+            break;
+          case "neutral":
+            setFeedback({...feedback, neutral: feedback.neutral + 1});
+            break;
+          case "bad":
+            setFeedback({...feedback, bad: feedback.bad + 1});
+            break;
+        }
+      }
+      return innerFunction
+    }
+  
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>give feedback</h1>
+      <Button onClickHandler={handleFeedback("good")} text="good"/>
+      <Button onClickHandler={handleFeedback("neutral")} text="neutral"/>
+      <Button onClickHandler={handleFeedback("bad")} text="bad"/>
+      <h1>statistics</h1>
+    <p>GOOD {feedback.good}</p>
+    <p>NEUTRAL {feedback.neutral}</p>
+    <p>BAD {feedback.bad}</p>
+    </div>
+
   )
 }
 
