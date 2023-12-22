69260dca5051a016488ef8a77611a60e20109a74
gros-pataplouf <sceres@pm.me>
2023-12-22 18:02:07 +0100

    Exercises 4.13.-4.14.

--- part_4/4.1-4.17_bloglist/controllers/blogs.js
+++ part_4/4.1-4.17_bloglist/controllers/blogs.js
@@ -13,4 +13,20 @@ blogRouter.post('/', async (request, response) => {
   response.status(201).json(result)
 })
 
+blogRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
+})
+
+blogRouter.put('/:id', async (request, response) => {
+  const { title, url, likes, author } = request.body
+  const requestedBlog = await Blog.findById(request.params.id)
+  if (!requestedBlog) {
+    return response.status(404).end() //this is to avoid that a deleted blog can be re-created by an update
+  }
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, { title, likes, author, url }, { new: true, runValidators: true })
+  response.status(200).send(updatedBlog)
+})
+
+
 module.exports = blogRouter
--- part_4/4.1-4.17_bloglist/tests/api.test.js
+++ part_4/4.1-4.17_bloglist/tests/api.test.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-undef */
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
@@ -8,80 +7,131 @@ mongoose.set('bufferTimeoutMS', 30000)
 
 const api = supertest(app)
 
-beforeAll(async () => {
+beforeEach(async () => {
   await Blog.deleteMany({})
   const testEntries = testData.map(entry => new Blog(entry))
   await Promise.all(testEntries.map(entry => entry.save()))
 })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+
+describe('GET /api/blogs', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('blog list length is length of initial data', async () => {
+    const response = await api
+      .get('/api/blogs')
+    const blogs = response.body
+    expect(blogs.length).toBe(testData.length)
+  })
+
+  test('the unique identifier of a blog is the id property', async () => {
+    const response = await api
+      .get('/api/blogs')
+    const ids = response.body.map(r => r.id)
+    ids.forEach(id => {
+      expect(id).toBeDefined()
+    })
+  })
+
 })
 
-test('blog list length is length of initial data', async () => {
-  const response = await api
-    .get('/api/blogs')
-  const blogs = response.body
-  expect(blogs.length).toBe(testData.length)
-})
+describe('POST /api/blogs', () => {
+  test('sending a post request to /api/blogs creates a new blog post', async () => {
+    const newBlog = new Blog({
+      title: 'JavaScript quirks',
+      author: 'Geeky nerds',
+      url: 'https://www.js-reexamined.fr',
+      likes: 9
+    })
+    await newBlog.save()
+    const response = await api
+      .get('/api/blogs')
+    expect(response.body.length).toBe(testData.length + 1)
+    const titles = response.body.map(r => r.title)
+    expect(titles).toContain(newBlog.title)
+  })
 
-test('the unique identifier of a blog is the id property', async () => {
-  const response = await api
-    .get('/api/blogs')
-  console.log(response.body)
-  const ids = response.body.map(r => r.id)
-  ids.forEach(id => {
-    expect(id).toBeDefined()
+  test(' if the likes property is missing from the request, it will default to the value 0', async () => {
+    const newBlog = new Blog({
+      title: 'JavaScript quirks',
+      author: 'Geeky nerds',
+      url: 'https://www.js-reexamined.fr'
+    })
+    const savedBlog = await newBlog.save()
+    expect(savedBlog.likes).toBe(0)
+  })
+
+  test(' if title or url properties are missing, backend responds with 400', async () => {
+    const newBlog = new Blog({
+      author: 'Geeky nerds',
+      url: 'https://www.js-reexamined.fr'
+    })
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+    const newBlog2 = new Blog({
+      title: 'Geeky nerds',
+      url: 'https://www.js-reexamined.fr'
+    })
+    await api
+      .post('/api/blogs')
+      .send(newBlog2)
+      .expect(400)
   })
 })
 
-test('sending a post request to /api/blogs creates a new blog post', async () => {
-  const newBlog = new Blog({
-    title: 'JavaScript quirks',
-    author: 'Geeky nerds',
-    url: 'https://www.js-reexamined.fr',
-    likes: 9
+describe('DELETE /api/blogs/:id', () => {
+  test('after deletion of valid id, blog list length is decremented by 1', async () => {
+    const responseBefore = await api
+      .get('/api/blogs')
+    const blogToBeDeleted = responseBefore.body[0]
+    await api.delete(`/api/blogs/${blogToBeDeleted.id}`)
+      .expect(204)
+    const responseAfter = await api.get('/api/blogs')
+    expect(responseAfter.body.length).toBe(testData.length - 1)
+    expect(responseAfter.body.map(elt => elt.title)).not.toContain(blogToBeDeleted.title)
   })
-  await newBlog.save()
-  const response = await api
-    .get('/api/blogs')
-  expect(response.body.length).toBe(testData.length + 1)
-  const titles = response.body.map(r => r.title)
-  expect(titles).toContain(newBlog.title)
+  test('deleting a malformatted id returns a 400', async () => {
+    const malformattedId = '123-fake-id'
+    await api.delete(`/api/blogs/${malformattedId}`)
+      .expect(400)
+  })
 })
 
-test(' if the likes property is missing from the request, it will default to the value 0', async () => {
-  const newBlog = new Blog({
-    title: 'JavaScript quirks',
-    author: 'Geeky nerds',
-    url: 'https://www.js-reexamined.fr'
+describe('PUT /api/blogs/:id', () => {
+  test('the number of likes can be updated by PUT request', async () => {
+    const response = await api.get('/api/blogs')
+    const postToBeUpdated = response.body[0]
+    postToBeUpdated.likes += 1
+    await api.put(`/api/blogs/${postToBeUpdated.id}`)
+      .send(postToBeUpdated)
+      .expect(200)
+      .expect(postToBeUpdated)
   })
-  const savedBlog = await newBlog.save()
-  expect(savedBlog.likes).toBe(0)
+  test('trying to update a non existing blog returns a 404', async () => {
+
+    const fakeId = '5b486d4057d0e42a3ca9c106'
+    const fakePost = {
+      title: 'Fake blog',
+      author: 'Elisabeth Holmes',
+      url: 'http://justafraud.com',
+      likes: -1000
+    }
+
+    await api.put(`/api/blogs/${fakeId}`)
+      .send(fakePost)
+      .expect(404)
+  })
+
 })
 
-test(' if title or url properties are missing, backend responds with 400', async () => {
-  const newBlog = new Blog({
-    author: 'Geeky nerds',
-    url: 'https://www.js-reexamined.fr'
-  })
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-  const newBlog2 = new Blog({
-    title: 'Geeky nerds',
-    url: 'https://www.js-reexamined.fr'
-  })
-  await api
-    .post('/api/blogs')
-    .send(newBlog2)
-    .expect(400)
-})
 
 afterAll(async () => {
   await mongoose.connection.close()
