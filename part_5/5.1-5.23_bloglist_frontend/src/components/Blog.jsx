5b6c5177df5ca70ad9bbe09fc1c4922de7b8bf6c
gros-pataplouf <sceres@pm.me>
2024-01-08 21:30:26 +0100

    5.7

--- part_5/5.1-5.23_bloglist_frontend/src/components/Blog.jsx
+++ part_5/5.1-5.23_bloglist_frontend/src/components/Blog.jsx
@@ -1,7 +1,55 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const flexStyle = {
+    display: "flex",
+  };
+
+  const buttonStyle = {
+    marginLeft: 15,
+    height: "min-content",
+    alignSelf: "center",
+  };
+  const [showDetails, setShowDetails] = useState(false);
+  const toggleDetails = () => setShowDetails(!showDetails);
+  {
+    return showDetails ? (
+      <div style={blogStyle}>
+        <div style={flexStyle}>
+          <p>
+            {blog.title} by {blog.author}
+          </p>
+          <button onClick={toggleDetails} style={buttonStyle}>
+            hide
+          </button>
+        </div>
+        <p>{blog.url}</p>
+        <div style={flexStyle}>
+          <p>likes {blog.likes}</p> <button style={buttonStyle}>like</button>
+        </div>
+        <p>{blog.user.name}</p>
+      </div>
+    ) : (
+      <div style={blogStyle}>
+        <div style={flexStyle}>
+          <p>
+            {blog.title} by {blog.author}
+          </p>
+          <button onClick={toggleDetails} style={buttonStyle}>
+            view
+          </button>
+        </div>
+      </div>
+    );
+  }
+};
 
 export default Blog;
