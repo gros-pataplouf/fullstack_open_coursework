17892c02dfa4f2835ce3fffeb477a3b94ccd7d4b
gros-pataplouf <sceres@pm.me>
2024-07-08 18:38:00 +0200

    7.1

--- part_7/routed-anecdotes/src/App.jsx
+++ part_7/routed-anecdotes/src/App.jsx
@@ -1,3 +1,4 @@
+import { Route, Routes, Link, BrowserRouter as Router } from 'react-router-dom'
 import { useState } from 'react'
 
 const Menu = () => {
@@ -6,9 +7,9 @@ const Menu = () => {
   }
   return (
     <div>
-      <a href='#' style={padding}>anecdotes</a>
-      <a href='#' style={padding}>create new</a>
-      <a href='#' style={padding}>about</a>
+      <Link to='/' style={padding}>anecdotes</Link>
+      <Link to='/create' style={padding}>create new</Link>
+      <Link to='/about' style={padding}>about</Link>
     </div>
   )
 }
@@ -30,7 +31,7 @@ const About = () => (
     <em>An anecdote is a brief, revealing account of an individual person or an incident.
       Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself,
       such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative.
-      An anecdote is "a story with a point."</em>
+      An anecdote is 'a story with a point.'</em>
 
     <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
   </div>
@@ -126,9 +127,11 @@ const App = () => {
     <div>
       <h1>Software anecdotes</h1>
       <Menu />
-      <AnecdoteList anecdotes={anecdotes} />
-      <About />
-      <CreateNew addNew={addNew} />
+      <Routes>
+        <Route path='/' element={<AnecdoteList anecdotes={anecdotes} />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/create' element={<CreateNew addNew={addNew} />}/>
+      </Routes>
       <Footer />
     </div>
   )
